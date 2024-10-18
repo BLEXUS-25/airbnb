4804e2f75f7a382423884e24d69940a12d4dba4b
BLEXUS-25 <satyamdhoot2005@gmail.com>
2024-10-18 16:48:03 +0530

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 
-app.set("view engine","ejs");
+app.set("view engine","ejs");;
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
