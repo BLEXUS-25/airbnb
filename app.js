eaefc0d831d04f041aa47014306f7d03ac4d10ee
BLEXUS-25 <satyamdhoot2005@gmail.com>
2024-10-24 23:57:51 +0530

    Server Side Validation Added

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
 
-app.set("view engine","ejs");;
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride("_method"));
-app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"/public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -22,57 +19,89 @@ async function main() {
 main().then(() => {
     console.log("connection successfull");
 })
-.catch((err) => {
-    console.log(err);
-});
+    .catch((err) => {
+        console.log(err);
+    });
 
-app.listen(8080,() => {
-    console.log("server is listening to port 8080");
-});
+app.set("view engine", "ejs");;
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/",(req,res) => {
+
+const validateListing = (req, res, next) => {
+    let { err } = listingSchema.validate(req.body);
+    if (err) {
+        let errMsg = err.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
+app.get("/", (req, res) => {
     res.send("HOME");
 });
 
 //INDEX ROUTE
-app.get("/listings", async (req,res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("listings/index.ejs",{allListings});
-});
+    res.render("listings/index.ejs", { allListings });
+}));
 
-app.get("/listings/new", (req,res) => {
+//NEW ROUTE
+app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 });
 
-//SHOW ROUTE 
-app.get("/listings/:id", async (req,res) => {
-    let {id} = req.params;
-    let listing = await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
-});
-
-app.post("/listings",async (req,res) => {
+//CREATE ROUTE
+app.post("/listings", validateListing, wrapAsync(async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-});
+}));
+
+//SHOW ROUTE 
+app.get("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+}));
+
 
 //UPDATE ROUTE
-app.get("/listings/:id/edit",async (req,res) => {
-    let {id} = req.params;
+app.get("/listings/:id/edit", validateListing, wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-});
+    res.render("listings/edit.ejs", { listing });
+}));
 
-app.put("/listings/:id", async (req,res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-});
+}));
 
-app.delete("/listings/:id", async (req,res) => {
-    let {id} = req.params;
+
+//DELETE ROUTE
+app.delete("/listings/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findByIdAndDelete(id);
     console.log(listing);
     res.redirect("/listings");
+}));
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page Not Found!"));
+});
+
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something went wrong!" } = err;
+    res.status(statusCode).render("error.ejs", { message });
+});
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
 });
