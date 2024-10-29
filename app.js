485c0090701b3ca3b2f46dd67edc45e0c95ad568
BLEXUS-25 <satyamdhoot2005@gmail.com>
2024-10-30 00:46:46 +0530

    review model added

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -41,6 +42,16 @@ const validateListing = (req, res, next) => {
     }
 }
 
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+}
+
 app.get("/", (req, res) => {
     res.send("HOME");
 });
@@ -58,7 +69,9 @@ app.get("/listings/new", (req, res) => {
 
 //CREATE ROUTE
 app.post("/listings", validateListing, wrapAsync(async (req, res) => {
+    console.log("Route mai agay hu ")
     const newListing = new Listing(req.body.listing);
+    console.log("new listing abb banayunga " + newListing)
     await newListing.save();
     res.redirect("/listings");
 }));
@@ -66,7 +79,7 @@ app.post("/listings", validateListing, wrapAsync(async (req, res) => {
 //SHOW ROUTE 
 app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    let listing = await Listing.findById(id);
+    let listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs", { listing });
 }));
 
@@ -89,9 +102,28 @@ app.put("/listings/:id", wrapAsync(async (req, res) => {
 app.delete("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findByIdAndDelete(id);
-    console.log(listing);
     res.redirect("/listings");
 }));
+
+
+//Review Create/Post Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+//Review Delete Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/listings/${id}`);
+}));
+
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
@@ -105,3 +137,4 @@ app.use((err, req, res, next) => {
 app.listen(8080, () => {
     console.log("server is listening to port 8080");
 });
+
